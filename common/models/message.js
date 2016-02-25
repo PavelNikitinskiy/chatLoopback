d77720082873dbfdfdfc2f49124720425466ec90
PavelNikitinskiy <paveln@elephantheadsoft.com>
2016-02-25 17:56:52 +0200

    message.js

--- common/models/message.js
+++ common/models/message.js
@@ -1,3 +1,12 @@
 module.exports = function(Message) {
+  Message.sendmessge = function(message,cb){
+    Message.create(
+      {'conten': message.content,
+      'userId':message.user_id,
+      'roomId':message.room_id,
+      'posted_at':new Date()},
+      function(err,mess){
 
+      });
+  };
 };
